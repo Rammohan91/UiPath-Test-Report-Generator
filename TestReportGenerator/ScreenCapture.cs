b63ae49bad2890bb94e62094311abc7cabad3bb1
Ganapathy <bganapathy@ceiamerica.com>
2020-06-05 01:25:40 +0530

    Screenshot functionality added

---
+++ TestReportGenerator/ScreenCapture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TestReportGenerator
+{
+    public enum EnumScreenCaptureMode
+    {
+        Screen,
+        Window
+    }
+
+    public class ScreenCapture
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public static Bitmap Capture(EnumScreenCaptureMode screenCaptureMode = EnumScreenCaptureMode.Window)
+        {
+            Rectangle bounds;
+
+            if (screenCaptureMode == EnumScreenCaptureMode.Screen)
+            {
+                bounds = Screen.GetBounds(Point.Empty);
+            }
+            else
+            {
+                var foregroundWindowsHandle = GetForegroundWindow();
+                var rect = new Rect();
+                GetWindowRect(foregroundWindowsHandle, ref rect);
+                bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            }
+
+            var result = new Bitmap(bounds.Width, bounds.Height);
+
+            using (var g = Graphics.FromImage(result))
+            {
+                g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            }
+
+            return result;
+        }
+    }
+}
